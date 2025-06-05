b091ce0efa4e17c64d4c9f2cf4f887fc6b445661
mkzt <myatthant68@gmail.com>
2025-06-05 20:52:13 +0630

    Adding PageNav for bootstrap

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,21 +1,41 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 const PageNav = () => {
   return (
-    <nav>
-        <ul>
-            <li>
-                <NavLink to='/'>Home</NavLink>
-            </li>
-            <li>
-                <NavLink to='/pricing'>Pricing</NavLink>
-            </li>
-            <li>
-                <NavLink to='/product'>Product</NavLink>
-            </li>
-        </ul>
-    </nav>
-  )
-}
+    <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
+      <NavLink className="navbar-brand" to="/">
+        MyApp
+      </NavLink>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-export default PageNav
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/pricing">
+              Pricing
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/product">
+              Product
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default PageNav;
